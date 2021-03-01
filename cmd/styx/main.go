62bc36d68094e0643853addbfb92a48ebd765e48
TurrpinA <alexandre.turpin@outlook.com>
2021-03-01 22:15:37 +0100

    Adding function for limiting requests

--- cmd/styx/main.go
+++ cmd/styx/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"golang.org/x/time/rate"
+)
+
+var token_bucket_size int = 3
+var token_refresh_rate_per_second rate.Limit = 1
 
 func main() {
-	fmt.Println("Hello World !")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", okHandler)
+
+	// Wrap the servemux with the limit middleware.
+	log.Println("Listening on :4000...")
+	//http.ListenAndServe(":4000", global_limit(mux))
+	http.ListenAndServe(":4000", user_limit(mux))
+}
+
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/guillaumebchd/styx
 
 go 1.15
+
+require golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
